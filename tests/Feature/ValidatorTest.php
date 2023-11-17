4d6266f32df51eebd8c6c55969ef091ce698eeab
Karungg <miftahfadilah71@gmail.com>
2023-11-17 08:10:22 +0700

    Error Message

--- tests/Feature/ValidatorTest.php
+++ tests/Feature/ValidatorTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use Tests\TestCase;
 
@@ -25,5 +26,27 @@ class ValidatorTest extends TestCase
 
         self::assertTrue($validator->passes());
         self::assertFalse($validator->fails());
+    }
+
+    public function testErrorMessage()
+    {
+        $data = [
+            'username' => 'miftahfadilah71',
+            'password' => 'miftah123'
+        ];
+
+        $rules = [
+            'username' => 'required',
+            'password' => 'required'
+        ];
+
+        $validator = Validator::make($data, $rules);
+
+        self::assertTrue($validator->passes());
+        self::assertFalse($validator->fails());
+
+        $message = $validator->getMessageBag();
+
+        Log::info($message->toJson(JSON_PRETTY_PRINT));
     }
 }
